c30a1ebb04f1b055712cd8ad34423aaad258d477
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2022-06-22 13:03:28 +0700

    update

--- config/routes.js
+++ config/routes.js
@@ -1,37 +1,41 @@
-const express = require("express");
-const controllers = require("../app/controllers");
-const apiRouter = express.Router();
-const uploadOnMemory = require("../app/services/uploadOnMemory")
+const express = require('express')
+const controllers = require('../app/controllers')
+const apiRouter = express.Router()
+const uploadOnMemory = require('../app/services/uploadOnMemory')
 
 apiRouter.post(
-  "/api/v1/auth/register",
-  controllers.api.v1.userController.register
-);
+  '/api/v1/auth/register',
+  controllers.api.v1.userController.register,
+)
 
 // UPDATE USER PROFILE
-apiRouter.put("/api/v1/users/:id", uploadOnMemory.single("gambar"), controllers.api.v1.userController.update);
+apiRouter.put(
+  '/api/v1/users/:id',
+  uploadOnMemory.single('gambar'),
+  controllers.api.v1.userController.update,
+)
 
 //login
-apiRouter.post("/api/v1/auth/login", controllers.api.v1.userController.login);
-apiRouter.post("/api/v1/auth/google", controllers.api.v1.userController.login);
+apiRouter.post('/api/v1/auth/login', controllers.api.v1.userController.login)
+apiRouter.post('/api/v1/auth/google', controllers.api.v1.userController.Google)
 
 // barang
-apiRouter.get("/api/v1/products", controllers.api.v1.productController.listAll);
+apiRouter.get('/api/v1/products', controllers.api.v1.productController.listAll)
 // tambah barang
 apiRouter.post(
-  "/api/v1/products",
-  uploadOnMemory.array("image", 4),
+  '/api/v1/products',
+  uploadOnMemory.array('image', 4),
   controllers.api.v1.userController.authorize,
-  controllers.api.v1.productController.addProduct
-);
+  controllers.api.v1.productController.addProduct,
+)
 // add new products category
 apiRouter.post(
-  "/api/v1/category",
-  controllers.api.v1.productController.addCategory
-);
+  '/api/v1/category',
+  controllers.api.v1.productController.addCategory,
+)
 
 // list all city
-apiRouter.get("/api/v1/cities", controllers.api.v1.cityController.listAllCity);
+apiRouter.get('/api/v1/cities', controllers.api.v1.cityController.listAllCity)
 
 /**
  * TODO: Delete this, this is just a demonstration of
@@ -43,7 +47,7 @@ apiRouter.get("/api/v1/cities", controllers.api.v1.cityController.listAllCity);
 //   );
 // });
 
-apiRouter.use(controllers.api.main.onLost);
-apiRouter.use(controllers.api.main.onError);
+apiRouter.use(controllers.api.main.onLost)
+apiRouter.use(controllers.api.main.onError)
 
-module.exports = apiRouter;
+module.exports = apiRouter
