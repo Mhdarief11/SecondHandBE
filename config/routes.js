47c3896e0c62ef3857b6343ebc63296d0cca6194
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2022-06-28 19:56:05 +0700

    update user profile

--- config/routes.js
+++ config/routes.js
@@ -16,10 +16,19 @@ apiRouter.post('/api/v1/auth/login', controllers.api.v1.userController.login)
 apiRouter.post('/api/v1/auth/google', controllers.api.v1.userController.Google)
 
 // GET USER DATA
-apiRouter.get(process.env.GETME, controllers.api.v1.userController.authorize, controllers.api.v1.userController.whoAmI)
+apiRouter.get(
+  '/api/v1/users/siapaSaya',
+  controllers.api.v1.userController.authorize,
+  controllers.api.v1.userController.whoAmI,
+)
 
 // UPDATE USER PROFILE
-apiRouter.put(process.env.PROFILE, controllers.api.v1.userController.authorize, uploadOnMemory.single("gambar"), controllers.api.v1.userController.update);
+apiRouter.put(
+  '/api/v1/users/update/:id',
+  controllers.api.v1.userController.authorize,
+  uploadOnMemory.single('gambar'),
+  controllers.api.v1.userController.update,
+)
 
 // apiRouter.put(process.env.PROFILEWITHOUTPIC, controllers.api.v1.userController.authorize, controllers.api.v1.userController.updateNP);
 
