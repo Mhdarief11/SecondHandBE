f6ee26054a4c2f6eddf7069d7f62869463f6821c
athallah aufar <athallahaufar741@gmail.com>
2022-06-29 22:58:20 +0700

    delete masih on process

--- app/services/productService.js
+++ app/services/productService.js
@@ -1,37 +1,40 @@
-const productRepository = require('../repositories/productRepository')
+const productRepository = require("../repositories/productRepository");
 
 module.exports = {
   async list() {
     try {
-      const barang = await productRepository.findAll()
-      const totalBarang = await productRepository.getTotalProducts()
+      const barang = await productRepository.findAll();
+      const totalBarang = await productRepository.getTotalProducts();
       return {
         barang,
         totalBarang,
-      }
+      };
     } catch (error) {
-      throw error
+      throw error;
     }
+  },
+  delete(id) {
+    return productsRepository.delete(id);
   },
 
   async addCategory(requestBody) {
-    return productRepository.addCategory(requestBody)
+    return productRepository.addCategory(requestBody);
   },
 
   async addProduct(requestBody) {
-    return productRepository.addProduct(requestBody)
+    return productRepository.addProduct(requestBody);
   },
 
   async addImageProduct(requestBody) {
-    return productRepository.addImageProduct(requestBody)
+    return productRepository.addImageProduct(requestBody);
   },
 
   async listCategory() {
     try {
-      const listcategory = await productRepository.listCategory()
-      return listcategory
+      const listcategory = await productRepository.listCategory();
+      return listcategory;
     } catch (error) {
-      throw error
+      throw error;
     }
   },
-}
+};
