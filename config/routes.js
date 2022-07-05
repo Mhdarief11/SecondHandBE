60478a9f671ceb1d457a37f77d903ec93acb142b
vitoradityafauzan <vitoraditya.f@gmail.com>
2022-07-05 20:46:50 +0700

    fixed route

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,9 @@ const controllers = require("../app/controllers");
 const apiRouter = express.Router();
 const uploadOnMemory = require("../app/services/uploadOnMemory");
 const cors = require("cors");
+const yaml = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = yaml.load("./openApi.yaml");
 
 apiRouter.use(cors());
 apiRouter.use(express.json());
@@ -17,8 +20,17 @@ apiRouter.post(
 apiRouter.post("/api/v1/auth/login", controllers.api.v1.userController.login);
 apiRouter.post("/api/v1/auth/google", controllers.api.v1.userController.Google);
 
+// GET USER DATA
+apiRouter.get('/api/v1/users/siapaSaya', controllers.api.v1.userController.authorize, controllers.api.v1.userController.whoAmI);
+
+apiRouter.get("/api/v1/users/profileImg/details/:id", controllers.api.v1.userController.getImg);
+
+// UPDATE USER PROFILE
+apiRouter.put('/api/v1/users/update/:id', controllers.api.v1.userController.authorize, uploadOnMemory.single("gambar"), controllers.api.v1.userController.update);
+
 // barang list all
 apiRouter.get("/api/v1/products", controllers.api.v1.productController.listAll);
+
 // get kategori
 apiRouter.get(
   "/api/v1/products",
@@ -52,6 +64,12 @@ apiRouter.post(
 
 // list all city
 apiRouter.get("/api/v1/cities", controllers.api.v1.cityController.listAllCity);
+
+// docs
+apiRouter.get('/api/v1/docs/swagger.json', (req, res) => {
+  res.status(200).json(swaggerDocument)
+})
+apiRouter.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 /**
  * TODO: Delete this, this is just a demonstration of
