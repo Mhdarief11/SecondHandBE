b94640702fca5acf8efdc3e4e31e1ed356e62008
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2022-07-11 21:35:00 +0700

    update homepage

--- app/models/kategori.js
+++ app/models/kategori.js
@@ -9,7 +9,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      kategori.hasOne(models.barang, { foreignKey: 'idkategori' })
     }
   }
   kategori.init(
